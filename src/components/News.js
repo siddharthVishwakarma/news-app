e2b898bbcd4ae384e7fef2cc8f2e7cbd48ba5a28
Siddharth Vishwakarma <sid.sv14@gmail.com>
2021-12-20 15:41:24 +0530

    fetch data from api

--- src/components/News.js
+++ src/components/News.js
@@ -2,38 +2,43 @@ import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 
 export class News extends Component {
+  constructor() {
+    super();
+    this.state = {
+      articles: [],
+      loading: false,
+    };
+  }
+
+  async componentDidMount() {
+    let url =
+      "https://newsapi.org/v2/everything?q=technology&apiKey=be30d809c74544209e01fe977372609f";
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    // console.log(parsedData);
+    this.setState({ articles: parsedData.articles });
+  }
+
   render() {
     return (
       <div>
-        <div class="container">
+        <div className="container my-3">
           <h1>This is a news component</h1>
-          <div class="row">
-            <div class="col">
-              <NewsItem />
-            </div>
-            <div class="col">
-              <NewsItem />
-            </div>
-            <div class="col">
-              <NewsItem />
-            </div>
-            <div class="col">
-              <NewsItem />
-            </div>
-          </div>
-          <div class="row my-3">
-            <div class="col">
-              <NewsItem />
-            </div>
-            <div class="col">
-              <NewsItem />
-            </div>
-            <div class="col">
-              <NewsItem />
-            </div>
-            <div class="col">
-              <NewsItem />
-            </div>
+          <div className="row">
+            {this.state.articles.map((ele) => {
+              return (
+                <div className="col-md-4 my-3 " key={ele.url}>
+                  <NewsItem
+                    title={ele.title ? ele.title.slice(0, 40) : ""}
+                    description={
+                      ele.description ? ele.description.slice(0, 80) : ""
+                    }
+                    imageUrl={ele.urlToImage}
+                    newsUrl={ele.url}
+                  />
+                </div>
+              );
+            })}
           </div>
         </div>
       </div>
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,18 +2,29 @@ import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
+    let { title, description, imageUrl, newsUrl } = this.props;
     return (
       <div>
         <div className="card" style={{ width: "18rem" }}>
-          <img src="..." className="card-img-top" alt="..." />
+          <img
+            src={
+              !imageUrl
+                ? "https://cdn.arstechnica.net/wp-content/uploads/2021/11/AirPower-mat-1-800x500-760x380.png"
+                : imageUrl
+            }
+            className="card-img-top"
+            alt="..."
+          />
           <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-            <a href="/" className="btn btn-primary">
-              Go somewhere
+            <h5 className="card-title">{title}...</h5>
+            <p className="card-text">{description}...</p>
+            <a
+              href={newsUrl}
+              target="_blank"
+              className="btn btn-primary btn-sm"
+              rel="noreferrer"
+            >
+              Read More
             </a>
           </div>
         </div>
